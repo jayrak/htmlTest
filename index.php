215f74895d69db789176c1c5b2ece96fd1459d1d
jayrak <jay001year@gmail.com>
2020-09-03 15:20:34 +0900

    first package

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+  <meta charset="UTF-8" />
+  <title>dontbecrazy</title>
+  <style>
+  h1 {
+    color: red;
+  }
+  </style>
+</head>
+  <body>
+    <h1>Hello world!!!</h1>
+    <p>はじめてのwebサイトへようこそ</p>
+    <a href="https://jp.investing.com/portfolio/?portfolioID=MDQ2ZDVrYz43aD4wMGU1Pw%3D%3D">株式見るかい？</a>
+  </body>
+</html>
